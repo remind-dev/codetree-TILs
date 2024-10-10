f5427052f20a8a627b0d969935bb7108400befe0
remind-dev <remind326@gmail.com>
2024-10-10 19:16:03 +0900

    Title: 1이 되는 순간까지, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241010/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def recursion(n):
+    if n == 1:
+        return 0
+
+    if n % 2 == 0:
+        return recursion(n // 2) + 1
+    else:
+        return recursion(n // 3) + 1
+
+print(recursion(n))
