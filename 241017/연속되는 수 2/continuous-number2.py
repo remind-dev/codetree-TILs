7e1e926b48c6fa3b8cb795e7a4a56995d87e7b9b
remind-dev <remind326@gmail.com>
2024-10-17 01:16:16 +0900

    Title: 연속되는 수 2, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/연속되는 수 2/continuous-number2.py
+++ 241017/연속되는 수 2/continuous-number2.py
@@ -1,15 +1,18 @@
 n = int(input())
-cnt = 0
+cnt = 1
+arr = [0 for _ in range(1001)]
 for i in range(n):
 
     cur = int(input())
     if i == 0:
-        cnt += 1
         temp = cur
         continue
 
-    if temp != cur:
+    if temp == cur:
         cnt += 1
+    else:
+        arr[temp] = max(cnt, arr[temp])
         temp = cur
+        cnt = 1
 
-print(cnt)
+print(max(arr))
