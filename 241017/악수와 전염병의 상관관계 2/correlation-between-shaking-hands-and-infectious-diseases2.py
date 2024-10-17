adaaa559e87dc345bb4736a02cba27a291863250
remind-dev <remind326@gmail.com>
2024-10-17 16:43:04 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,26 @@
+n, k, p, t = map(int, input().split())
+
+element = [tuple(map(int, input().split())) for _ in range(t)]
+element.sort()
+
+parasite = [0 for _ in range(n+1)]
+parasite[p] = k
+
+ans = [0 for _ in range(n+1)]
+ans[p] = 1
+
+for s, n1, n2 in element:
+    if parasite[n1] and not parasite[n2]:
+        parasite[n1] -= 1
+        parasite[n2] = p
+        ans[n2] = 1
+    elif parasite[n2] and not parasite[n1]:
+        parasite[n2] -= 1
+        parasite[n1] = p
+        ans [n1] = 1
+    elif parasite[n1] and parasite[n1]:
+        parasite[n1] -= 1
+        parasite[n2] -= 1
+
+for i in ans[1:]:
+    print(i, end='')
