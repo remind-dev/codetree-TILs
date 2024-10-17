3da5e3625255569b40141df53675aaddeab3c721
remind-dev <remind326@gmail.com>
2024-10-17 15:38:55 +0900

    Title: 벌금은 누구에게, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/벌금은 누구에게/who-will-pay.py
+++ 241017/벌금은 누구에게/who-will-pay.py
@@ -1,10 +1,13 @@
 n, m, k = map(int, input().split())
 
 arr = [0 for _ in range(n + 1)]
+ans = -1
 for i in range(1,m+1):
     p = int(input())
 
     arr[p] += 1
     if arr[p] >= k:
-        print(p)
+        ans = p
         break
+
+print(ans)
