f253215ebf931d76dd29201a3161841ae1d7cc60
remind-dev <remind326@gmail.com>
2024-10-19 18:51:33 +0900

    Title: 금 채굴하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241019/금 채굴하기/gold-mining.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+
+
+def find_gold(k, x, y):
+    cnt = 0
+    for dx in range(-k, k+1):
+        for dy in range(-k, k+1):
+            if (abs(dx) + abs(dy)) <= k:
+                nx = x + dx
+                ny = y + dy
+
+                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 1:
+                    cnt += 1
+    return cnt
+
+
+
+
+for k in range(1, n):
+    pay = k*k + (k+1)*(k+1)
+
+    for x in range(n):
+        for y in range(n):
+            cnt_gold = find_gold(k, x, y)
+            if pay <= cnt_gold*m:
+                ans = max(ans, cnt_gold)
+
+print(ans)
