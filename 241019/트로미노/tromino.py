8e1512e808173bb9f8a8e0f21576ee8a877afaf8
remind-dev <remind326@gmail.com>
2024-10-19 17:35:05 +0900

    Title: 트로미노, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241019/트로미노/tromino.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+def rotate_90(arr):
+    temp = zip(*arr[::-1])
+    return [list(i) for i in temp]
+
+
+ans = 0
+temp_arr = arr
+for _ in range(4):
+    temp_arr = rotate_90(temp_arr)
+
+    for i in range(n):
+        for j in range(m):
+
+            if i+1 < n and j+1 < m:
+                shape1 = temp_arr[i][j] + temp_arr[i+1][j] + temp_arr[i+1][j+1]
+
+            if j+2 < m:
+                shape2 = temp_arr[i][j] + temp_arr[i][j+1] + temp_arr[i][j+2] 
+
+            ans = max(ans, shape1, shape2)
+
+print(ans)
