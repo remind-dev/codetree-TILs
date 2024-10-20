cb03869e6afa69a261dff0413dd6ffc56cbfad2d
remind-dev <remind326@gmail.com>
2024-10-20 17:07:55 +0900

    Title: 최단 Run Length 인코딩, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241020/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 241020/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -6,14 +6,20 @@ def shift():
     return string.insert(0, string.pop())
 
 def check():
-    length = 2
+    length = ''
+    cnt = 1
     for i in range(1, n):
         if string[i] == string[i - 1]:
-            continue
+            cnt += 1
         else:
-            length += 2
+            length = string[i-1] + str(cnt)
 
-    return length
+    if cnt > 1:
+        length = string[i] + str(cnt)
+        return len(length)
+    else:
+        return len(length) + 2
+
 
 MIN = 21
 for _ in range(n):
