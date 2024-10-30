0cc517d46babc856e5a0c7eaee78ffdfeae6d656
remind-dev <remind326@gmail.com>
2024-10-30 16:27:01 +0900

    Title: 아름다운 수, Time: 473ms, Memory: 29MB, Status: Passed - CodeTree

--- 241030/아름다운 수/beautiful-number.py
+++ 241030/아름다운 수/beautiful-number.py
@@ -1,29 +1,35 @@
 n = int(input())
 
+answer = []
+
 def is_beautiful(number):
-    # 주어진 숫자가 아름다운 수인지 확인
     i = 0
     length = len(number)
     while i < length:
         count = 1
-        while i + 1 < length and number[i] == number[i + 1]:
+        while i + 1 < length and number[i] == number[i+1]:
+            if count == number[i]:
+                break
+
             count += 1
             i += 1
-        if int(number[i]) != count:
+
+        if count != number[i]:
             return False
         i += 1
+
     return True
 
-def count_beautiful_numbers(current):
-    # 현재 문자열이 길이 n에 도달했을 때 아름다운 수인지 확인
-    if len(current) == n:
-        return 1 if is_beautiful(current) else 0
+def beutiful_num(cnt):
+    if cnt == n:
+        return 1 if is_beautiful(answer) else 0
     
     count = 0
     for digit in range(1, 5):
-        count += count_beautiful_numbers(current + str(digit))
+        answer.append(digit)
+        count += beutiful_num(cnt+1)
+        answer.pop() 
+
     return count
 
-# 길이 n의 아름다운 수의 개수 출력
-result = count_beautiful_numbers("")
-print(result)
+print(beutiful_num(0))
