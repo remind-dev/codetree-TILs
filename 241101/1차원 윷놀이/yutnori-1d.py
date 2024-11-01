004c320f8fb35b7113584877b724df439025fe67
remind-dev <remind326@gmail.com>
2024-11-01 15:37:23 +0900

    Title: 1차원 윷놀이, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241101/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,32 @@
+n, m, k = map(int,input().split())
+distance = list(map(int, input().split()))
+
+hores = [0] * k
+
+
+def arrive():
+    point = 0
+    for dis in hores:
+        if dis >= m:
+            point += 1
+
+    return point
+
+
+ans = 0
+def yutnori(cnt):
+    global ans
+
+    if cnt == n:
+        ans = max(ans, arrive())
+        return
+
+    for i in range(k):
+        for dis in distance:
+            hores[i] += dis
+            yutnori(cnt + 1)
+            hores[i] -= dis
+
+
+yutnori(0)
+print(ans)
