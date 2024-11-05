22aefe430170d2ed6c7737b973921f20624aa902
remind-dev <remind326@gmail.com>
2024-11-05 15:11:28 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 241105/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,27 +1,48 @@
-n = int(input())
+import sys
 
-def is_valid(seq):
-    cont_n = len(seq) // 2
-    for i in range(1, cont_n + 1):
-        if seq[-i:] == seq[-2 * i:-i]:  # 최근 추가한 부분만 검사
+# 변수 선언 및 입력
+n = int(input())
+numbers = [4, 5, 6]
+
+series = []
+
+# 가능한 수열인지 여부를 판별합니다.
+def is_possible_series():
+    # 가능한 연속 부분 수열의 길이 범위를 탐색합니다.
+    length = 1
+    while True:
+        # 연속 부분 수열의 시작과 끝 인덱스를 설정하여 줍니다.
+        start1, end1 = len(series) - length, len(series) - 1
+        start2, end2 = start1 - length, start1 - 1
+
+        if start2 < 0:
+            break
+
+        # 인접한 연속 부분 수열이 같은지 여부를 확인합니다.
+        if series[start1:end1 + 1] == series[start2:end2 + 1]:
             return False
+
+        length += 1
+    
     return True
 
-def backtrack(cnt):
-    global ans
 
+def find_min_series(cnt):
+    # n개의 숫자가 선택됐을 때 불가능한 수열인 경우 탐색을 종료합니다.
+    # 가능한 수열인 경우 이를 출력하고 프로그램을 종료합니다.
     if cnt == n:
-        print("".join(map(str, seq)))  # 가능한 수열을 찾았으면 출력
-        return True  # 정답을 찾았으므로 종료
+        for elem in series:
+            print(elem, end = "")
+        sys.exit(0)
     
-    for i in range(4, 7):  # 사전순으로 가장 앞선 수열을 찾기 위해 4, 5, 6 순서로 시도
-        seq.append(i)
-        if is_valid(seq):  # 유효성 검사 후 백트래킹 진행
-            if backtrack(cnt + 1):  # 정답을 찾았으면 더 이상 탐색하지 않음
-                return True
-        seq.pop()  # 다음 숫자를 추가하기 전 상태로 복구
-    
-    return False
+    # 사용 가능한 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
+    for number in numbers:
+        series.append(number)
+        # 해당 시점까지 만들 수열이 조건을 만족하는 경우에만
+        # 탐색을 진행합니다.
+        if is_possible_series():
+            find_min_series(cnt + 1)
+        series.pop()
+        
 
-seq = []
-backtrack(0)
+find_min_series(0)
