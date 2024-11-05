bddcdbdf0329f84ad0a69a1596991e84ef514350
remind-dev <remind326@gmail.com>
2024-11-05 15:08:42 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 241105/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,41 +1,27 @@
 n = int(input())
 
-def check():
+def is_valid(seq):
     cont_n = len(seq) // 2
-
-    for i in range(1, cont_n+1):
-        for j in range(len(seq)-i):
-
-            if seq[j:j+i] == seq[j+i:j+i+i]:
-                return False
-
+    for i in range(1, cont_n + 1):
+        if seq[-i:] == seq[-2 * i:-i]:  # 최근 추가한 부분만 검사
+            return False
     return True
 
-def example_check():
-
-    if check():
-        print(1)
-    else:
-        print(0)
-
-seq = []
-ans = []
 def backtrack(cnt):
     global ans
 
     if cnt == n:
-        if check() and not ans:
-            ans = seq[:]
-        return
-
-    for i in range(4, 7):
-        if ans:
-            return
+        print("".join(map(str, seq)))  # 가능한 수열을 찾았으면 출력
+        return True  # 정답을 찾았으므로 종료
+    
+    for i in range(4, 7):  # 사전순으로 가장 앞선 수열을 찾기 위해 4, 5, 6 순서로 시도
         seq.append(i)
-        backtrack(cnt + 1)
-        seq.pop()
+        if is_valid(seq):  # 유효성 검사 후 백트래킹 진행
+            if backtrack(cnt + 1):  # 정답을 찾았으면 더 이상 탐색하지 않음
+                return True
+        seq.pop()  # 다음 숫자를 추가하기 전 상태로 복구
+    
+    return False
 
+seq = []
 backtrack(0)
-
-for i in ans:
-    print(i, end='')
