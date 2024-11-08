6436ecd96bda22cd4c20564c061f473eeacd5c54
remind-dev <remind326@gmail.com>
2024-11-08 18:08:04 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 59ms, Memory: 16MB, Status: Passed - CodeTree

--- 241108/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 241108/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -2,33 +2,20 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-def calc(nums, idx_list):
-    sum1 = sum(nums)
-
-    sum2 = 0
-    for i in range(len(arr)):
-        if not i in idx_list:
-            sum2 += arr[i]
-
-    return abs(sum1 - sum2)
-
 ans = float('inf')
-nums = []
 
-def backtrack(curr_idx, cnt, idx_list):
+def backtrack(curr_idx, cnt, diff):
     global ans
 
     if curr_idx == 2*n:
         if cnt == n:
-            ans = min(ans, calc(nums, idx_list))
+            ans = min(ans, abs(diff))
         return
 
 
-    nums.append(arr[curr_idx])
-    backtrack(curr_idx + 1, cnt + 1, idx_list + [curr_idx])
-    nums.pop()
+    backtrack(curr_idx + 1, cnt + 1, diff + arr[curr_idx])
 
-    backtrack(curr_idx + 1, cnt, idx_list)
+    backtrack(curr_idx + 1, cnt, diff - arr[curr_idx])
 
-backtrack(0, 0, [])
+backtrack(0, 0, 0)
 print(ans)
